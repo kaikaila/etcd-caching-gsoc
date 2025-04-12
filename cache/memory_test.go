fc9b4356e974dbe26e1d9ca9e8d8286743a6f807
kaikaila <lyk2772@126.com>
2025-04-11 22:14:38 -0700

    memory_test are generalized to any class which implements cache interface

--- cache/memory_test.go
+++ cache/memory_test.go
@@ -1,11 +1,10 @@
 package cache
 
-// 建一个以 _test.go 结尾的文件，写函数名是 TestXxx(t *testing.T)，Go 会自动识别成测试用例。
 import "testing"
 
-func TestMemoryCache_GetSet(t *testing.T) {
-    c := NewMemoryCache()
-
+func RunBasicCacheTests(t *testing.T, c Cache) {
+    /* this test should be applied to any class which implements cache interface */
+    // Basic Set and Get test 
     c.Set("foo", "bar")
 
     val, ok := c.Get("foo")
@@ -15,4 +14,23 @@ func TestMemoryCache_GetSet(t *testing.T) {
     if val != "bar" {
         t.Fatalf("Expected value 'bar', got '%s'", val)
     }
+
+    // Overwriting existing key
+    c.Set("foo", "baz")
+    val, ok = c.Get("foo")
+    if !ok || val != "baz" {
+        t.Fatalf("Expected overwritten value 'baz', got '%s'", val)
+    }
+
+    // Getting non-existent key
+    val, ok = c.Get("notfound")
+    if ok {
+        t.Fatalf("Expected key 'notfound' to be missing, got '%s'", val)
+    }
+
+    // TODO: use multiple goroutines to concurrently Set and Get, and verify no data race or inconsistency occurs
+}
+
+func TestMemoryCache_GetSet(t *testing.T) {
+    RunBasicCacheTests(t, NewMemoryCache())
 }
