b5232bdd7fe02fc3f65a0c55399ef41d43a56d51
kaikaila <lyk2772@126.com>
2025-04-12 13:33:43 -0700

    add unit test for EventSink

---
+++ cache/memory_eventsink_test.go
@@ -0,0 +1,20 @@
+package cache
+
+import "testing"
+
+func TestMemoryCache_EventSink(t *testing.T) {
+	s := NewMemoryCache()
+	
+	c := s.(CacheWithSink)
+	c.HandlePut("foo", "bar")
+	val, ok := c.Get("foo")
+	if !ok || val != "bar" {
+		t.Errorf("expected key 'foo' to have value 'bar', got val=%q, ok=%v", val, ok)
+	}
+
+	c.HandleDelete("foo")
+	val, ok = c.Get("foo")
+	if !ok || val != "" {
+		t.Errorf("expected key 'foo' to be cleared, got val=%q, ok=%v", val, ok)
+	}
+}
